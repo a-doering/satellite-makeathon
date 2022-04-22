add811e0f6ba2be2836989fcc122bb70529904d6
Andreas <35858164+a-doering@users.noreply.github.com>
2022-04-22 23:20:06 +0200

    Add worldmap

--- viz/eurosat_geospatial.py
+++ viz/eurosat_geospatial.py
@@ -3,6 +3,7 @@ from rasterio.plot import show
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import geopandas as gpd
 
 
 def show_geospatial_image(file_path):
@@ -16,9 +17,29 @@ def show_image(file_path):
     jpg = np.array(Image.open(file_path))
     plt.imshow(jpg)
 
+def plot_world_map(lat, lon, labels):
+    # From GeoPandas, our world map data
+    worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    worldmap.plot(color="lightgrey", ax=ax)
+
+    # Plotting our Impact Energy data with a color map
+    x = lon
+    y = lat
+    z = 60
+    plt.scatter(x, y, s=z, c="k", alpha=0.6, vmin=0, cmap='autumn')
+
+    # Creating axis limits and title
+    plt.xlim([-180, 180])
+    plt.ylim([-90, 90])
+    plt.title("World map")
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.show()
 
 if __name__ == "__main__":
-    i = 1
+    i = 1500
     # jpg
     file_path = f"data/EuroSAT/2750/AnnualCrop/AnnualCrop_{i}.jpg"
     show_image(file_path)
@@ -29,3 +50,12 @@ if __name__ == "__main__":
     tiff = rasterio.open(file_path)
     print(tiff.bounds)
     show_geospatial_image(file_path)
+    plt.pause(0.5)
+
+    #TODO: fix crs system and add labels to legend
+    print(tiff.crs)
+    labels = ['AnnualCrop','Forest', 'HerbaceousVegetation', 'Highway',"Industrial", 'Pasture', 'PermanentCrop','Residential', 'River', 'SeaLake']
+    plot_world_map([tiff.bounds.top / 100000],[tiff.bounds.left / 100000],  labels)
+
+
+
