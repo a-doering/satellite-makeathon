ec73da1bc1b4176830bc0585a768f14f8a95dcb7
Andreas <35858164+a-doering@users.noreply.github.com>
2022-04-23 13:56:23 +0200

    Add worldmap

--- viz/eurosat_geospatial.py
+++ viz/eurosat_geospatial.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import pandas as pd
 import geopandas as gpd
+import glob
+import os
 
 
 def show_geospatial_image(file_path):
@@ -23,23 +25,41 @@ def plot_world_map(gdf):
     worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
     print(worldmap.crs)
 
+    print(gdf["Latitude"].max(), gdf["Latitude"].min())
+    print(gdf["Longitude"].max(), gdf["Longitude"].min())
+    #gdf.to_crs(worldmap.crs)
+    worldmap.to_crs(gdf.crs)
+
     fig, ax = plt.subplots(figsize=(12, 6))
     worldmap.plot(color="lightgrey", ax=ax)
 
     # Plotting our Impact Energy data with a color map
     # x = lon
     # y = lat
-    # z = 60
-    # plt.scatter(x, y, s=z, c="k", alpha=0.6, vmin=0, cmap='autumn')
+    z = 60
+    plt.scatter(gdf.Longitude, gdf.Latitude, s=z, c="k", alpha=0.6, vmin=0, cmap='autumn')
     print(gdf.head())
-    gdf.plot(ax=ax, color="red")
+    #gdf.plot(ax=ax, color="red")
     # Creating axis limits and title
-    plt.xlim([-180, 180])
-    plt.ylim([-90, 90])
+    # plt.xlim([-180, 180])
+    # plt.ylim([-90, 90])
     plt.title("World map")
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
     plt.show()
+
+def get_all_lat_lon(classes=["*"]):
+    lat = []
+    lon = []
+    for c in classes:
+        for file in glob.glob(f"data/EuroSATallBands/ds/images/remote_sensing/otherDatasets/sentinel_2/tif/{c}/*"):
+            if not os.path.isfile(file):
+                continue
+            tiff = rasterio.open(file)
+            lat.append(tiff.bounds.top  / 100000)
+            lon.append(tiff.bounds.left / 100000)
+    return (lat, lon)
+
 
 def create_gdf(lat, lon, crs):
 
@@ -53,9 +73,9 @@ def create_gdf(lat, lon, crs):
     return gdf
 
 if __name__ == "__main__":
-    i = 1900
+    i = 1
     # jpg
-    file_path = f"data/EuroSAT/2750/Industrial/Industrial_{i}.jpg"
+    file_path = f"data/EuroSAT/2750/AnnualCrop/AnnualCrop_{i}.jpg"
     show_image(file_path)
     plt.pause(0.5)
 
@@ -63,16 +83,25 @@ if __name__ == "__main__":
     file_path = f"data/EuroSATallBands/ds/images/remote_sensing/otherDatasets/sentinel_2/tif/AnnualCrop/AnnualCrop_{i}.tif"
     tiff = rasterio.open(file_path)
     print(tiff.bounds)
+    print(tiff.meta)
+    print(tiff.bounds)
     show_geospatial_image(file_path)
     plt.pause(0.5)
 
-    #TODO: labels to legend
-    labels = ['AnnualCrop','Forest', 'HerbaceousVegetation', 'Highway',"Industrial", 'Pasture', 'PermanentCrop','Residential', 'River', 'SeaLake']
+    print(tiff.transform)
+    x, y = tiff.xy(tiff.height // 2, tiff.width //2)
+    print(x,y)
+    print(tiff.crs.wkt)
+
+    # #TODO: labels to legend
+    # labels = ['AnnualCrop','Forest', 'HerbaceousVegetation', 'Highway',"Industrial", 'Pasture', 'PermanentCrop','Residential', 'River', 'SeaLake']
     
     lat = [tiff.bounds.top  / 100000]
     lon = [tiff.bounds.left / 100000]
+    #lat, lon = get_all_lat_lon(["AnnualCrop"])
     crs = tiff.crs
     gdf = create_gdf(lat, lon, crs)
+    # TODO: projection error!
     plot_world_map(gdf)
 
 
