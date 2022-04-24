0c389d6fa1b43d9882cc5d7886c015ddc1825583
atmorw <57006214+atmorw@users.noreply.github.com>
2022-04-24 09:47:41 +0200

    Update index.js

--- web mock/index.js
+++ web mock/index.js
@@ -1,13 +1,40 @@
-let showIn = function(event) {
-    let image = document.getElementById('inImg');
-    console.log(event)
-    image.src = URL.createObjectURL(event.target.files[0]);
+let modals = document.getElementsByClassName('modals')
+let modalBtns = document.getElementsByClassName('modalBtns')
+let closeSpans = document.getElementsByClassName('close')
+
+for (let modalBtn of modalBtns) {
+    modalBtn.onclick = function() {
+        let modalId = modalBtn.dataset.modal
+        let modal = document.getElementById(modalId)
+        modal.style.display = "block";
+    }
+}
+
+for (let span of closeSpans) {
+    span.onclick = function() {
+        let modalId = span.dataset.modal
+        let modal = document.getElementById(modalId)
+        modal.style.display = "none";
+    }
+}
+
+let showIn = function() {
+    wait(1000)
+    document.getElementById('inImg').style.visibility = 'visible';
 };
 
-console.log(document)
 let showOut = function() {
-    wait(1500)
+    document.getElementById('wait').style.visibility = 'visible';
+    document.getElementById('loading').style.visibility = 'visible';
+    setTimeout(load, 2000);
+}
+
+let load = function() {
+    document.getElementById('wait').style.visibility = 'hidden';
+    document.getElementById('loading').style.visibility = 'hidden';
+    document.getElementById('arrow').style.visibility = 'visible';
     document.getElementById('outImg').style.visibility = 'visible';
+    document.getElementById('legend').style.visibility = 'visible';
 }
 
 let wait = function(ms) {
